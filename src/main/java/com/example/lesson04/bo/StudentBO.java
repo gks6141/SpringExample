b0faef950a75ea61b8cb75891abb2e3f3d779dff
DESKTOP-POO0TJK\user <test@test.com>
2024-07-03 18:52:14 +0900

    JPA DB update ,delete , JPQL select

--- src/main/java/com/example/lesson04/bo/StudentBO.java
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -1,6 +1,7 @@
 package com.example.lesson04.bo;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -44,5 +45,40 @@ public class StudentBO {
 				.build();
 		
 		return studentRepository.save(student);
+	}
+	
+	//update
+	//input: id, dreamJob
+	//output: StudentEntity ,null
+	public StudentEntity updateStudentDreamJobById(int id, String dreamJob) {
+		//기존 데이터 조회 => StudentEntity
+		StudentEntity student = studentRepository.findById(id).orElse(null);
+		//엔티티의 데이터 값을 변경해놓는다.
+		if(student != null) {
+			student = student.toBuilder() //toBuilder는 기존 필드값은 유지하고 일부만 변경
+				.dreamJob(dreamJob)
+				.build();// 꼭 객체에 다시 저장!
+			
+			//update
+			//save 요청
+			student = studentRepository.save(student);
+		}
+		
+		return student;
+	}
+	
+	//delete
+	//input:id
+	//output:X
+	public void deleteStudentById(int id) {
+		//방법1)
+//		StudentEntity student = studentRepository.findById(id).orElse(null);
+//		if (student != null) {
+//			studentRepository.delete(student);
+//		}
+		
+		//방법2)
+		Optional<StudentEntity> studentOptional = studentRepository.findById(id);
+		studentOptional.ifPresent(s -> studentRepository.delete(s));
 	}
 }
--- src/main/java/com/example/lesson07/Lesson07Ex01RestController.java
+++ src/main/java/com/example/lesson07/Lesson07Ex01RestController.java
@@ -28,4 +28,20 @@ public class Lesson07Ex01RestController {
 				
 	}
 	
+	//U:update
+	@GetMapping("/update")
+	public StudentEntity update() {
+		//id가 7번 dreamJob 변경(기획자)
+		return studentBO.updateStudentDreamJobById(7, "기획자");
+		
+	}
+	
+	//D:delete
+	@GetMapping("/delete")
+	public String delete() {
+		//id가 7번인 데이터 삭제
+		studentBO.deleteStudentById(7);
+		
+		return "삭제 성공";
+	}
 }
